6d830b07ac9b84c2aba0a6456f5f82b52c424723
uchennaponuigbo <uchenna.p.onuigbo@gmail.com>
2022-10-06 23:08:54 -0700

    fixed unparsable errors on my end

---
+++ the-lost-penguin/Assets/Scripts/KentoStats.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public int Level = 1;
+    public int currentEXP;
+
+
+    public int currentHP;
+    public int maxHP;
+    //merge changes in main to bring over hp script
+    //check GDD for skills we planned on having kento upgrade...
+    //then come back to tutorial
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
